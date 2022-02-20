f2f7673331b1bbadaf16ff2f546e969ac2025013
abiyyu03 <abiyyu652@gmail.com>
2022-02-20 23:19:30 +0700

    add loginController

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function loginCashier()
+    {
+        return view('cashier.auth.login');
+    }
+
+    public function loginOwner()
+    {
+        return view('owner.auth.login');
+    }
+
+    public function authCashier(Request $request)
+    {
+        $data = $request->only('email','password');
+        dd($data);
+        // if(Auth()->attempt($data))
+        // {
+        //     return redirect()->route('/owner');
+        // }
+    }
+}
