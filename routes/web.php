fe55c209ff4bbafeba2cc7ce7d38baf07adbfcd1
abiyyu03 <abiyyu652@gmail.com>
2022-03-19 14:34:10 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,48 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/',[DashboardController::class,'index']);
-// owner
-// Route::get('/owner',[ProductController::class,'indexOwner']);
-// Route::get('/owner/login',[Auth\LoginController::class,'loginOwner']);
-// Route::post('/owner/login',[Auth\LoginController::class,'authOwner'])->name('owner.login');
 
-// // cashier
-// Route::get('/cashier',[ProductController::class,'indexCashier']);
-// Route::get('/cashier/login',[Auth\LoginController::class,'loginCashier']);
-// Route::post('/cashier/login',[Auth\LoginController::class,'authCashier'])->name('cashier.login');
-// Route::get('/product','app/Http/Controllers/ProductController@index');
+Route::middleware('owner')->group(function(){
+    Route::get('/',[DashboardController::class,'index']);
+});
+
+Route::middleware('supervisor')->group(function(){
+    Route::get('/asdd', function () {
+        return 'supervisor';    
+    });
+});
+
+Route::middleware('cashier')->group(function(){
+    // Route::get('/',[DashboardController::class,'index']);
+    Route::get('/asd', function () {
+        return 'cashier';    
+    });
+});
+
+//Authentication's route 
+Route::get('/login',[Auth\LoginController::class,'login']);
+Route::post('/login',[Auth\LoginController::class,'login_process'])->name('logins.login');
+Route::get('/register',[Auth\RegisterController::class,'register']);
+Route::post('/register',[Auth\RegisterController::class,'register_process'])->name('registers.register');
+Route::get('/logout',[Auth\LoginController::class,'logout']);
+
+// Route::get('/login_owner',[Auth\LoginController::class,'login_owner']);
+// Route::post('/login_owner',[Auth\LoginController::class,'login_owner_process'])->name('login_owner.login');
+// Route::get('/logout_owner',[Auth\LoginController::class,'logout_owner']);
+
+// Route::get('/login_supervisor',[Auth\LoginController::class,'login_supervisor']);
+// Route::post('/login_supervisor',[Auth\LoginController::class,'login_supervisor_process'])->name('login_supervisor.login');
+// Route::get('/logout_supervisor',[Auth\LoginController::class,'logout_supervisor']);
+
+// Route::get('/login_cashier',[Auth\LoginController::class,'login_cashier']);
+// Route::post('/login_cashier',[Auth\LoginController::class,'login_cashier_process'])->name('login_cashier.login');
+// Route::get('/logout_cashier',[Auth\LoginController::class,'logout_cashier']);
+
+// Route::get('/register',[Auth\RegisterController::class,'register']);
+// Route::post('/register',[Auth\RegisterController::class,'register_process'])->name('registers.register');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+// require __DIR__.'/auth.php';
