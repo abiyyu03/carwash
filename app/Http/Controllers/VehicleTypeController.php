cb7551bc64466526e7865e27b1342dd1511d5533
abiyyu03 <abiyyu652@gmail.com>
2022-03-31 11:44:58 +0700

    create data in vehicle type features

--- app/Http/Controllers/VehicleTypeController.php
+++ app/Http/Controllers/VehicleTypeController.php
@@ -7,77 +7,39 @@ use Illuminate\Http\Request;
 
 class VehicleTypeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        return view('vehicle_type.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $vehicleType_data = new VehicleType();
+        $vehicleType_data->vehicle_type = $request->get('vehicle_type');
+        $vehicleType_data->save();
+        return redirect()->back();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\VehicleType  $vehicleType
-     * @return \Illuminate\Http\Response
-     */
     public function show(VehicleType $vehicleType)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\VehicleType  $vehicleType
-     * @return \Illuminate\Http\Response
-     */
     public function edit(VehicleType $vehicleType)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\VehicleType  $vehicleType
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, VehicleType $vehicleType)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\VehicleType  $vehicleType
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(VehicleType $vehicleType)
     {
         //
