c20d7148c4dde5a861e18f42227fa7c00392c203
abiyyu03 <abiyyu652@gmail.com>
2022-03-31 09:56:44 +0700

    add method to productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\{Product,Product_category};
 
 class ProductController extends Controller
 {
+    function index()
+    {
+        return view('product.index');
+    }
 
+    function store(Request $request)
+    {
+        //
+    }
 }
