ba5ca2ffc717f28285bc5bfeb9e8940b2fac2e9c
m fitrah <rajwaizati12@gmail.com>
2022-04-20 22:43:17 +0700

    transaction detail

---
+++ database/migrations/2022_04_20_153202_create_transaction_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->id('id_transaction_detail');
+            $table->integer('amount');
+            $table->integer('price');
+            $table->integer('total');
+            $table->forigenid('transaction_id');
+            $table->forigenid('item_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+}
