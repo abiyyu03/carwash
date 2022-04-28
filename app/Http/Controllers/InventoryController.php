414d9b18f261ba620e403d23f61bd0f854474a81
abiyyu03 <abiyyu652@gmail.com>
2022-04-28 14:11:09 +0700

    add datatables to inventory

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Inventory;
+use DataTables;
 
 class InventoryController extends Controller
 {
     function index()
     {
         return view('inventory.index');
+    }
+
+    function inventoryJson()
+    {
+        $inventory_data = Inventory::get();
+        return Datatables::of($inventory_data)->addIndexColumn()->make(true);
+            // ->addColumn('productCategory',function (Product $product){
+            //     return $product->productCategory->category_name;
+            // })
+            // ->toJson();
+    }
+    function store(Request $request)
+    {
+        $inventory_data = $request->all();
+        Inventory::create($inventory_data);
+        return redirect()->back();
+
     }
 }
