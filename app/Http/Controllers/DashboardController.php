e74c2104d65d23df387a828136e664fb0af44125
abiyyu03 <abiyyu652@gmail.com>
2022-04-28 14:06:36 +0700

    just a plain dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{Customer, VehicleType};
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        // return view('product.index');
+        return view('dashboard.index');
     }
 }
