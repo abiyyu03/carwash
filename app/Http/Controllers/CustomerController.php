ca05c01dc98a04b1510c7775c3367f67d35977b3
abiyyu03 <abiyyu652@gmail.com>
2022-04-28 14:05:53 +0700

    add Json data source of customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Customer;
+use DataTables;
 
 class CustomerController extends Controller
 {
     function index()
     {
         return view ('customer.index');
+    }
+
+    function customerJson()
+    {
+        $customer_data = Customer::get();
+        return Datatables::of($customer_data)->addIndexColumn()->make(true);
     }
     
 }
