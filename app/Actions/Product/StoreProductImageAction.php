f2167f4eeb2b82e2d9cce4931aff0ba21bb5aee2
abiyyu03 <abiyyu652@gmail.com>
2022-05-17 11:15:41 +0700

    separate store product's image

---
+++ app/Actions/Product/StoreProductImageAction.php
@@ -0,0 +1,24 @@
+<?php 
+namespace App\Actions\Product;
+
+use Illuminate\Http\Request;
+use App\Models\{Product, User, Role, WorkDetail, TransactionDetail};
+// use App\Actions\Employee\{StoreUserAction, StorePhotoAction};
+use Image;
+
+class StoreProductImageAction
+{
+    public $filename;
+    function executeProduct(Request $request)
+    {
+        $image = $request->file('product_image');
+        $this->filename = $image->getClientOriginalName();
+
+        $image->move(public_path().'/img/img_temp/',$this->filename);
+        $image_compressed = Image::make(public_path().'/img/img_temp/'.$this->filename);
+        $image_compressed->fit(240,120);
+        $image_compressed->save(public_path('/img/product/'.$this->filename));
+        unlink(public_path('/img/img_temp/'.$this->filename));
+
+    }
+}
