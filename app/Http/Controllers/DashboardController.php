9eb606585ef2ff1fac54f1c555829fb386b6e10e
abiyyu03 <abiyyu652@gmail.com>
2022-05-17 11:27:09 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\{Customer, VehicleType};
+use App\Models\{Customer, VehicleType, Transaction, Product};
 
 class DashboardController extends Controller
 {
-    public function index()
+    function index()
     {
-        return view('dashboard.index');
+        $transaction_data = $this->getAllTransaction();
+        $transactionPending_data = $this->getAllTransaction();
+        $product_data = $this->getProduct();
+        return view('dashboard.index',compact('transaction_data','transactionPending_data','product_data'));
+    }
+
+    function getPendingTransaction()
+    {
+        return Transaction::with('customer')
+                ->where('transaction_status','pending')
+                ->get();
+    }
+    function getAllTransaction()
+    {
+        return Transaction::with('customer')->get();
+    }
+
+    function getProduct()
+    {
+        return Product::with('productCategory')->get();
     }
 }
