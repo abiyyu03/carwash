e24f5a497799ab93498cf46fe428d57d74b156ca
abiyyu03 <abiyyu652@gmail.com>
2022-05-17 11:23:00 +0700

    workdetail

--- app/Models/WorkDetail.php
+++ app/Models/WorkDetail.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class WorkDetail extends Model
 {
     use HasFactory;
+    protected $table = "work_details";
+    protected $primaryKey = "id_work_detail";
+    protected $fillable = ['employee_id','commission','transaction_detail_id','date'];
+
+    function employee()
+    {
+        return $this->belongsTo('App\Models\Employee','employee_id');
+    }
+
+    function transactionDetail()
+    {
+        return $this->belongsTo('App\Models\TransactionDetail','transaction_detail_id');
+    }
 }
