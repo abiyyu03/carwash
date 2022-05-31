38e69357f47b6ba5a3abad24edf88c68cf45cfe2
abiyyu03 <abiyyu652@gmail.com>
2022-05-31 21:56:45 +0700

    add client validation on transaction #1

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -5,12 +5,12 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0">Dashboard</h1>
+        <h1 class="m-0">Daftar Transaksi</h1>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard v1</li>
+          <li class="breadcrumb-item active">Transaksi</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
@@ -120,7 +120,7 @@
                 <!-- better use select with search bar -->
                 <select name="id_customer" class="form-control" required>
                     <!-- <option>-</option> -->
-                    <option>-</option>
+                    <option value="" disabled>-</option>
                     @foreach($customer_data as $customer)
                       <option value="{{ $customer->id_customer }}">{{ $customer->customer_name }}</option>
                     @endforeach
