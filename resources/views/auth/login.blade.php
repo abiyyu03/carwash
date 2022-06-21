129aaa79662583eb3a9ca47b0dbd1a2ca546b6f8
abiyyu03 <abiyyu652@gmail.com>
2022-06-21 11:26:55 +0700

    fix login ux

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,7 +33,7 @@
         <form action="{{route('logins.login')}}" method="post">
           @csrf
           <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email" name="email">
+            <input type="email" class="form-control" placeholder="Email" value="{{ old('email')}}" name="email" required>
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -41,7 +41,7 @@
             </div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password" name="password">
+            <input type="password" class="form-control" placeholder="Password" name="password" required>
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
