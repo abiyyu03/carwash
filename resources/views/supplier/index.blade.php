bc380b3943d2ec82b03d23b1d5361cee130e83c8
abiyyu03 <abiyyu652@gmail.com>
2022-06-24 13:52:57 +0700

    supplier

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -6,12 +6,12 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0">Supplier</h1>
+        <h1 class="m-0">Data Supplier</h1>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard v1</li>
+          <li class="breadcrumb-item active">Inventory</li>
+          <li class="breadcrumb-item active">Supplier</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
@@ -38,35 +38,17 @@
   <div class="card mt-3">
     <div class="card-body">
       <div class="table-responsive">
-        <table class="table data-inventoryDetail">
+        <table class="table data-supplier">
           <thead>
             <tr>
               <th>#</th>
-              <th>Nama Barang</th>
-              <th>Harga Barang</th>
-              <th>Jumlah Barang</th>
-              <th>Nama Item / Produk</th>
-              <th>Nama / Nama Toko</th>
+              <th>Nama Toko</th>
               <th>Kontak Toko</th>
               <th>Aksi</th>
             </tr>
           </thead>
           <tbody>
-            @foreach($inventoryDetail_data as $inventoryDetail)
-            <tr>
-              <td>{{ $loop->iteration }}</td>
-              <td>{{ $inventoryDetail->inventory_detail_name }}</td>
-              <td>{{ $inventoryDetail->inventory_detail_price }}</td>
-              <td>{{ $inventoryDetail->inventory_detail_amount }}</td>
-              <td>{{ ($inventoryDetail->inventory != NULL) ? @$inventoryDetail->inventory->inventory_name : @$inventoryDetail->product->product_name }}</td>
-              <td>{{ $inventoryDetail->supplier_name }}</td>
-              <td>{{ $inventoryDetail->supplier_contact }}</td>
-              <td>
-                <a href="#" id="editButton" data-toggle="modal" data-target="#editModal" class="btn btn-warning"><i class="fas fa-edit"></i> Edit</a> 
-                <a href="/supplier/delete/{{ $inventoryDetail->id_inventory_detail }}" id="deleteButton" class="btn btn-danger deleteButton"><i class="fas fa-trash-alt"></i> Delete</a> 
-              </td>
-            </tr>
-            @endforeach
+            
           </tbody>
         </table>
       </div>
@@ -84,44 +66,14 @@
         <div class="modal-body">
           <form action="{{route('inventoryDetail.store')}}" method="POST" enctype="multipart/form-data">
             @csrf
-            {{-- <div class="form-group">
+            <div class="form-group">
               <label for="supplier_name" class="col-form-label">Nama Supplier / Toko <sup class="text-danger">*</sup></label>
               <input type="text" class="form-control" id="supplier_name" value="{{old('supplier_name')}}" name="supplier_name" required>
-            </div> --}}
-            <div class="form-group">
-              <label for="inventory_detail_name" class="col-form-label">Nama Barang</label>
-              <input type="text" class="form-control" id="inventory_detail_name" value="{{old('inventory_detail_name')}}" name="inventory_detail_name" required>
             </div>
             <div class="form-group">
-              <label for="inventory_detail_amount" class="col-form-label">Jumlah Barang</label>
-              <input type="text" class="form-control" id="inventory_detail_amount" value="{{old('inventory_detail_amount')}}" name="inventory_detail_amount" required>
-            </div>
-            <div class="form-group">
-              <label for="inventory_detail_price" class="col-form-label">Harga Barang</label>
-              <input type="number" class="form-control" id="inventory_detail_price" value="{{old('inventory_detail_price')}}" name="inventory_detail_price" required>
-            </div>
-            <div class="form-group">
-              <label for="inventory_id" class="col-form-label">Nama Inventory</label>
-              <select name="inventory_id" id="" class="form-control">
-                <option value="">-</option>
-                @foreach($inventory_data as $item)
-                  <option value="{{ $item->id_inventory }}">{{ $item->inventory_name }}</option>
-                @endforeach
-              </select>
-            </div>
-            <div class="form-group">
-              <label for="product_id" class="col-form-label">Nama Produk</label>
-              <select name="product_id" id="" class="form-control">
-                <option value="">-</option>
-                @foreach($product_data as $product)
-                  <option value="{{ $product->id_product }}">{{ $product->product_name }}</option>
-                @endforeach
-              </select>
-            </div>
-            {{-- <div class="form-group">
               <label for="supplier_contact" class="col-form-label">Kontak Supplier</label>
               <input type="text" class="form-control" id="supplier_contact" name="supplier_contact">{{old('supplier_contact')}}</textarea>
-            </div> --}}
+            </div>
             <div class="modal-footer form-group">
               <button type="submit" class="btn btn-info">Tambah Supplier</button>
             </div>
@@ -179,7 +131,45 @@
 @push('addon-scripts')
 <script>
   $(document).ready(function(){
-    $('.data-supplier').DataTable();
+    $('.data-supplier').DataTable({
+      processing:true,
+      serverSide:true,
+      ajax:"{{route('supplier')}}",
+      // ajax:{
+      //   url
+      //   data:{status:status}
+      // },
+      columns:[
+        {data:"DT_RowIndex",name:"DT_RowIndex", orderable:false, searchable:false},
+        {data:"supplier_name",name:"supplier_name"},
+        {data:"supplier_contact",name:"supplier_contact"},
+        {
+          data:"action",
+          name:"action"
+        }
+      ],
+      drawCallback: function(settings){
+          $('#deleteButton').on("click",function(e){
+          e.preventDefault();
+          var url = $(this).attr('href');
+          console.log(url);
+          swal.fire({
+            title: 'Apakah Kamu yakin ingin menghapus data ini ?',
+            text: "Data yang terhapus tidak bisa di kembalikan!",
+            icon: 'warning',
+            // buttons: ["Cancel","Yakin!"],
+            showCancelButton: true,
+            // confirmButtonColor: '#3085d6',
+            // cancelButtonColor: '#d33',
+            confirmButtonText: 'Yakin !'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              window.location.href = url;
+            }
+          });
+        });
+      }
+    });
     $('.deleteButton').on("click",function(event){
         event.preventDefault();
         var url = $(this).attr('href');
