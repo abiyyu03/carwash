af5be99e9a51e36cea30b96af8389c67da23c70d
abiyyu03 <abiyyu652@gmail.com>
2022-06-29 21:50:44 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\{Customer, VehicleType, Transaction, TransactionDetail, Product};
+use App\Models\{Customer, VehicleType, Transaction, TransactionDetail, Product, ProductCategory};
 use Lava;
 use DataTables;
 
@@ -11,23 +11,24 @@ class DashboardController extends Controller
 {
     function index()
     {
-        $chart = Lava::DataTable();
-        $chart->addDateColumn('Day of Month')
-            ->addNumberColumn('Projected')
-            ->addNumberColumn('Official');
+        // $chart = Lava::DataTable();
+        // $chart->addDateColumn('Day of Month')
+        //     ->addNumberColumn('Projected')
+        //     ->addNumberColumn('Official');
 
-        // Random Data For Example
-        for ($a = 1; $a < 30; $a++) {
-            $chart->addRow([
-            '2015-10-' . $a, rand(800,1000), rand(800,1000)
-            ]);
-        }
-        Lava::LineChart('MyStocks', $chart);
+        // // Random Data For Example
+        // for ($a = 1; $a < 30; $a++) {
+        //     $chart->addRow([
+        //     '2015-10-' . $a, rand(800,1000), rand(800,1000)
+        //     ]);
+        // }
+        // Lava::LineChart('MyStocks', $chart);
         
         $transaction_data = $this->getAllTransaction();
         $transactionPending_data = $this->getAllTransaction();
         $product_data = $this->getProduct();
-        return view('dashboard.index',compact('transaction_data','transactionPending_data','product_data'));
+        $categoryProduct_data = $this->getProductCategory();
+        return view('dashboard.index',compact('transaction_data','transactionPending_data','product_data','categoryProduct_data'));
     }
 
     function getPendingTransaction()
@@ -44,5 +45,10 @@ class DashboardController extends Controller
     function getProduct()
     {
         return Product::with('productCategory')->get();
+    }
+
+    function getProductCategory()
+    {
+        return ProductCategory::count();
     }
 }
